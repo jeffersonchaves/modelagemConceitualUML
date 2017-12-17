0cf37b2884eb5af8c7672c72928273bfc7c1f746
Jeffeson Chaves <jefferson.chaves@ifc.edu.br>
2017-12-17 19:43:00 -0200

    Projeto criado

---
+++ src/main/java/br/edu/ifc/araquari/CursoModelagemApplication.java
@@ -0,0 +1,12 @@
+package br.edu.ifc.araquari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoModelagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoModelagemApplication.class, args);
+	}
+}
